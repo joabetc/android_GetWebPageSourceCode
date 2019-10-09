4a0f50c5db0e86f0ae9b8acf4f36d02b943682e9
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-09 15:40:17 -0300

    Adding items to the spinner

--- app/src/main/java/com/joabe/getwebpagesourcecode/MainActivity.java
+++ app/src/main/java/com/joabe/getwebpagesourcecode/MainActivity.java
@@ -3,12 +3,38 @@ package com.joabe.getwebpagesourcecode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner protocolSpinner = findViewById(R.id.protocol_spinner);
+
+        ArrayAdapter<CharSequence> protocolAdapter = ArrayAdapter.createFromResource(this,
+                R.array.protocol_array, android.R.layout.simple_spinner_item);
+        protocolAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        if (protocolSpinner != null) {
+            protocolSpinner.setOnItemSelectedListener(this);
+            protocolSpinner.setAdapter(protocolAdapter);
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
